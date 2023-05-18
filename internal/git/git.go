4195ff2c35008b5f7eefc42d6b811e81e66940f1
Luca Scalzotto <luca@scorpiac.com>
2023-05-18 17:44:16 +0200

    Working git symbols

--- internal/git/git.go
+++ internal/git/git.go
@@ -11,8 +11,37 @@ func Git() string {
 	if rawStatus, ok := internal.Command("git", "status", "--porcelain", "-b"); ok {
 		s := parseStatus(rawStatus)
 
-		return ansi.Color(ansi.Magenta, " "+branchIcon+" "+s.branch)
+		return ansi.Color(ansi.Magenta, " "+branchIcon+" "+s.branch) + statusText(s)
 	}
 
 	return ""
+}
+
+func statusText(s status) string {
+	symbols := ""
+
+	if s.untracked {
+		symbols += "?"
+	}
+	if s.staged {
+		symbols += "+"
+	}
+	if s.unstaged {
+		symbols += "!"
+	}
+	if s.renamed {
+		symbols += "»"
+	}
+	if s.deleted {
+		symbols += "✘"
+	}
+	if s.unmerged {
+		symbols += "="
+	}
+
+	if symbols != "" {
+		symbols = ansi.Color(ansi.Red, " ["+symbols+"]")
+	}
+
+	return symbols
 }
--- internal/git/status.go
+++ internal/git/status.go
@@ -3,7 +3,13 @@ package git
 import "strings"
 
 type status struct {
-	branch string
+	branch    string
+	untracked bool
+	staged    bool
+	unstaged  bool
+	renamed   bool
+	deleted   bool
+	unmerged  bool
 }
 
 func parseStatus(raw string) status {
@@ -17,12 +23,40 @@ func parseStatus(raw string) status {
 	return result
 }
 
+// See: https://git-scm.com/docs/git-status#_output
 func parseStatusLine(s *status, line string) {
-	code := strings.TrimSpace(line[:2])
+	code := line[:2]
 
-	switch code {
-	case "##":
+	if code == "##" {
 		branch, _, _ := strings.Cut(line[3:], "...")
 		s.branch = branch
+		return
+	}
+	if code == "??" {
+		s.untracked = true
+		return
+	}
+
+	codeX := rune(code[0]) // Index
+	codeY := rune(code[1]) // Working tree
+
+	if codeX == 'M' || codeX == 'T' || codeX == 'A' {
+		s.staged = true
+	}
+
+	if codeY == 'M' || codeY == 'T' {
+		s.unstaged = true
+	}
+
+	if codeX == 'R' || codeY == 'R' {
+		s.renamed = true
+	}
+
+	if codeX == 'D' || codeY == 'D' {
+		s.deleted = true
+	}
+
+	if codeX == 'U' || codeY == 'U' || code == "DD" || code == "AA" {
+		s.unmerged = true
 	}
 }
