cd18802bee7136d17f7da125aa16c45383e69d91
Luca Scalzotto <luca@scorpiac.com>
2023-05-23 20:39:02 +0200

    Fix zero-width ansi codes

--- gokart.zsh-theme
+++ gokart.zsh-theme
@@ -1,11 +1,13 @@
 #!/usr/bin/env zsh
 
 # Get the path to this file, make it absolute and strip the filename.
-# See:  https://stackoverflow.com/questions/9901210/bash-source0-equivalent-in-zsh/28336473#28336473
+# See: https://stackoverflow.com/questions/9901210/bash-source0-equivalent-in-zsh/28336473#28336473
 GOKART_PROMPT_DIR=${${(%):-%x}:A:h}
 
 gokart_prompt_precmd() {
     export EXIT_CODE=$?
+    export GOKART_SHELL=zsh
+
     PS1=$("$GOKART_PROMPT_DIR/gokart" ps1)
     PS2=$("$GOKART_PROMPT_DIR/gokart" ps2)
 }
--- internal/ansi/ansi.go
+++ internal/ansi/ansi.go
@@ -1,11 +1,17 @@
 package ansi
 
+import "os"
+
 const esc = "\x1B"
 const reset = "0"
 
 // See: https://stackoverflow.com/a/19731390/2831123
 const zshZeroWidthStart = "%{"
 const zshZeroWidthEnd = "%}"
+
+// See: https://unix.stackexchange.com/a/504152/176402
+const bashZeroWidthStart = "\x01"
+const bashZeroWidthEnd = "\x02"
 
 func mode(mode string) string {
 	return zeroWidth(esc + "[" + mode + "m")
@@ -16,5 +22,8 @@ func Reset() string {
 }
 
 func zeroWidth(s string) string {
-	return zshZeroWidthStart + s + zshZeroWidthEnd
+	if os.Getenv("GOKART_SHELL") == "zsh" {
+		return zshZeroWidthStart + s + zshZeroWidthEnd
+	}
+	return bashZeroWidthStart + s + bashZeroWidthEnd
 }
