56e098380f3050ecc1dea8229b51237643cd5f96
Luca Scalzotto <luca@scalzotto.nl>
2026-02-01 16:19:21 +0100

    Separate SemVerRange struct

--- internal/semVer/semVer.go
+++ internal/semVer/semVer.go
@@ -5,17 +5,23 @@ import (
 )
 
 type SemVer struct {
-	major    int
-	minor    int
-	patch    int
+	major int
+	minor int
+	patch int
+}
+
+type SemVerRange struct {
+	SemVer
 	operator string
 }
 
 type Operator string
 
-var Tilde Operator = "~"
-var Caret Operator = "^"
+var Gt Operator = ">"
+var Gte Operator = ">="
+var Lt Operator = "<"
+var Lte Operator = "<="
 
 func (s SemVer) String() string {
-	return fmt.Sprintf("%s%d.%d.%d", s.operator, s.major, s.minor, s.patch)
+	return fmt.Sprintf("%d.%d.%d", s.major, s.minor, s.patch)
 }
