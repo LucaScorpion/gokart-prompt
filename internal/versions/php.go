a5ac14bc2f6e3186e09c07265626d84145e13d00
Luca Scalzotto <luca@scalzotto.nl>
2026-02-01 15:39:44 +0100

    Get require php version from composer.json

--- internal/versions/php.go
+++ internal/versions/php.go
@@ -1,6 +1,8 @@
 package versions
 
 import (
+	"encoding/json"
+	"gokart-prompt/internal"
 	"gokart-prompt/internal/ansi"
 	"strings"
 )
@@ -26,4 +28,23 @@ var Php = section{
 		*/
 		return "v" + strings.SplitN(output, " ", 3)[1]
 	},
+
+	expectedVersionFn: func() string {
+		str := internal.UpsearchWdFileContents("composer.json")
+
+		var data composerJson
+		if err := json.Unmarshal([]byte(str), &data); err != nil {
+			return ""
+		}
+
+		return data.Require.Php
+	},
+}
+
+type composerJson struct {
+	Require composerJsonRequire `json:"require"`
+}
+
+type composerJsonRequire struct {
+	Php string `json:"php"`
 }
